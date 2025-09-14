7eee4e330876b005f201fe59b5caf4a82e2ffb5e
frazakhtar <87436611+frazakhtar@users.noreply.github.com>
2025-09-14 23:57:09 +0530

    Add routes and fully responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import Navbar from './Components/NavBar/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './Components/Home/HomePage';
+import About from './Components/About/About';
 
 function App() {
   return (
     <div className="App">
-      <h1>This is Working!</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/*" element={<h1>This is Error 404</h1>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/NavBar/Navbar.js
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+  MenuList,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import LocalLibraryIcon from "@mui/icons-material/LocalLibrary";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const [anchorNav, setAnchorNav] = useState(null);
+  const openMenu = (e) => {
+    setAnchorNav(e.currentTarget);
+  };
+  const closeMenu = () => {
+    setAnchorNav(null);
+  };
+  return (
+    <AppBar position="static" sx={{ backgroundColor: '#3776A1' }}>
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          sx={{ display: { xs: "none", md: "flex" } }}
+        >
+          <LocalLibraryIcon />
+        </IconButton>
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ flexGrow: 1, ml: 2, display: { xs: "none", md: "flex" } }}
+        >
+          My Library App
+        </Typography>
+        <Box sx={{ display: { xs: "none", md: "flex" } }}>
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/about">
+            About
+          </Button>
+          <Button color="inherit">User Info</Button>
+        </Box>
+
+        {/* This is for responsive screens */}
+
+        <Box
+          sx={{
+            display: { xs: "flex", md: "none" },
+            width: "100%",
+            alignItems: "center",
+          }}
+        >
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            sx={{ display: { xs: "flex", md: "none" } }}
+          >
+            <LocalLibraryIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+          >
+            My Library App
+          </Typography>
+          <IconButton
+            onClick={(e) => openMenu(e)}
+            size="large"
+            edge="end"
+            color="inherit"
+            sx={{ display: { xs: "flex", md: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            anchorEl={anchorNav}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorNav)}
+            onClose={closeMenu}
+            sx={{ display: { xs: "flex", md: "none" } }}
+          >
+            <MenuList sx={{m:1,p:1}} onClick={() => closeMenu()}>
+              <MenuItem component={Link} to={"/"}>
+                Home
+              </MenuItem>
+              <MenuItem component={Link} to={"/about"}>
+                About
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
